64bf14c39f912cc3601649992a49c40f5b8e29a9
Nick Dyer <nbdyer@gmail.com>
2014-10-09 23:58:28 +0100

    Exercise 16 readfile testing

--- ex16.rb
+++ ex16.rb
@@ -7,12 +7,12 @@ puts "If you do want that, hit RETURN."
 $stdin.gets
 
 puts "Opening the file..."
-target = open(filename, 'w')
+target = open(filename, 'w+')
 
 puts "Truncating the file. Goodbye!"
 target.truncate(0)
 
-puts "Now I'm going to ask you for three lines."
+puts "Now I'm going to ask you some text."
 
 print "line 1: "
 line1 = $stdin.gets.chomp
@@ -23,12 +23,13 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("#{line1}\n#{line2}\n#{line3}\n")
 
 puts "And finally, we close it."
 target.close
+
+readfile = ARGV.first
+target2 = open(readfile, 'r')
+
+puts "Read it:"
+print target2.read
